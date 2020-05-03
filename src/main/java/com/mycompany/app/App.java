813088643884f8033f594b0215ef18db88ee9cdb
SARATH RACHERLA <sharathsaee@gmail.com>
2020-05-03 08:31:37 +0530

    20200503-0830-Commit for src and pom.xml
    20200503-0830-Commit for src and pom.xml

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World, changes brough by Sarath for v2.0" );
+    }
+}
